18cc4b1b27edb23671db02cf4cfb81ab2d0b5c81
lili herculano <paul.riicky762@gmail.com>
2023-01-15 23:26:57 -0300

    update inserido na serviceProduct

--- src/Services/serviceProdutos.js
+++ src/Services/serviceProdutos.js
@@ -10,4 +10,15 @@ async function createProduct(name, provider, category, composition, unityPrice, 
     conn.end()
 }
 
-export default {createProduct}
+async function updateProduct(name, provider, category, composition, unityPrice, actualQtd, minQtd, maxQtd, id){
+    const sql = 'update cadastro_loja set nome_produto = ?, FK_id_fornecedor = ?, categoria_produto = ?, composicao_produto = ?, preco_unitario = ?, quantidade_produto = ?, quantidade_minima = ?, quantidade_maxima = ? where id_produto = ?'
+
+    const data = [name, provider, category, composition, unityPrice, actualQtd, minQtd, maxQtd, id]
+
+    const conn = await database.connect()
+    conn.query(sql,data)
+    conn.end()
+
+}
+
+export default {createProduct, updateProduct}
